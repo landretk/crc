f831eb8a2d160e1ebb37526b589f6ffeb599b8bf
Keegan Landreth <landreth.keegan@gmail.com>
2025-07-17 21:53:38 -0600

    document some common crcs

--- gen_crc.py
+++ gen_crc.py
@@ -3,6 +3,24 @@
 from enum import Enum, IntEnum
 import argparse
 
+INFO = '''
+Some common CRC parameters:
+(Check is with ascii string "123456789")
+
+CRC8/SAE-J1850:    0x1D forward, init=0xff, final=0xff, check=0x4b
+CRC8/AUTOSAR:      0x2F forward, init=0xff, final=0xff, check=0xdf
+
+CRC16/FALSE-CCITT: 0x1021 forward, init=0xffff, check=0x29b1
+CRC16/KERMIT:      0x1021 reflect, init=0x0000, check=0x2189
+CRC16/ARC:         0x8005 reflect, init=0x0000, check=0xbb3d
+
+CRC32:             0x04C11DB7 reflect, init=0xffffffff, final=0xffffffff, check=0xcbf43926
+CRC32/AUTOSAR:     0xF4ACFB13 reflect, init=0xffffffff, final=0xffffffff, check=0x1697d06a
+
+CRC64/ECMA-182:    0x42F0E1EBA9EA3693 reflect, init=0xffffffffffffffff,
+                       final=0xffffffffffffffff, check=0x995dc9bbdf1939fa
+'''
+
 class CRCType(Enum):
     Forward = 1,
     Reflected = 2
@@ -38,12 +56,12 @@ def generate_crc(width, type: CRCType, poly):
     return table
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="CRC table generator")
+    parser = argparse.ArgumentParser(description="CRC table generator", epilog=INFO, formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument('width', help="Bit width of CRC polynomial")
     parser.add_argument('polynomial', help="CRC polynomial e.g. 0x1021 for CRC16-CCITT, 0x04C11DB7 for CRC32")
     parser.add_argument('--name', default=None, help="Name of CRC table")
-    parser.add_argument('--reflected', action='store_true', help="Generate reflected table e.g. CRC16-ARC")
+    parser.add_argument('--reflect', action='store_true', help="Generate reflected table e.g. CRC16-ARC")
 
     args = parser.parse_args()
 
@@ -58,7 +76,7 @@ if __name__ == "__main__":
     else:
         name = args.name
 
-    crc_type = CRCType.Reflected if args.reflected else CRCType.Forward
+    crc_type = CRCType.Reflected if args.reflect else CRCType.Forward
 
     polynomial = int(args.polynomial, 0)
 
